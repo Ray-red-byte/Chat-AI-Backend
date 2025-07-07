0d625b37125e427ce8a2fa23b5ea1ec7ea1cfe99
Ray-red-byte <ray67672@gmail.com>
2025-07-08 02:11:18 +0800

    add: implement message handling with WebSocket support, including message storage and conversation validation

---
+++ internal/repositories/message.go
@@ -0,0 +1,84 @@
+// chatapp/internal/repositories/messageRepository.go
+
+package repositories
+
+import (
+	"chat-app/internal/models"
+	"chat-app/utils"
+	"context"
+	"errors"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type MessageRepository struct {
+	MongoMsgCol   *mongo.Collection
+	MongoConvoCol *mongo.Collection
+}
+
+func NewMessageRepository(
+	mongoMsgCol *mongo.Collection,
+	mongoConvoCol *mongo.Collection,
+) *MessageRepository {
+	return &MessageRepository{
+		MongoMsgCol:   mongoMsgCol,
+		MongoConvoCol: mongoConvoCol,
+	}
+}
+
+// GetConversationByID retrieves a conversation by its ID
+func (r *MessageRepository) GetConversationByID(conversationID string) (*models.Conversation, error) {
+	// Convert string to ObjectId
+	objectID, err := primitive.ObjectIDFromHex(conversationID)
+	if err != nil {
+		utils.Logger.Printf("Invalid conversationID format: %s\n", conversationID)
+		return nil, err
+	}
+
+	var conversation models.Conversation
+	err = r.MongoConvoCol.FindOne(context.TODO(), bson.M{"_id": objectID}).Decode(&conversation)
+	if err != nil {
+		utils.Logger.Printf("Failed to find conversation by ID %s: %v\n", conversationID, err)
+		return nil, err
+	}
+	return &conversation, nil
+}
+
+// SaveMessage saves a message to MongoDB.
+func (r *MessageRepository) SaveMessage(message models.Message) error {
+	// Set default values for fields if not provided
+	if message.Feedback == nil {
+		defaultFeedback := "No feedback" // Default feedback message
+		message.Feedback = &defaultFeedback
+	}
+	if message.ThumbUp == 0 {
+		message.ThumbUp = 0 // Default value for ThumbUp
+	}
+	if message.InputURL == "" {
+		message.InputURL = "" // Default empty string
+	}
+	if message.OutputURL == "" {
+		message.OutputURL = "" // Default empty string
+	}
+
+	// Check if the MessageCollection is initialized
+	if r.MongoMsgCol == nil {
+		utils.Logger.Printf("Error: Message collection is not initialized")
+		return errors.New("message collection is not initialized")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := r.MongoMsgCol.InsertOne(ctx, message)
+	if err != nil {
+		utils.Logger.Printf("Failed to save message: %v\n", err)
+		return err
+	}
+
+	utils.Logger.Printf("Message saved: %+v", message)
+	return nil
+}
